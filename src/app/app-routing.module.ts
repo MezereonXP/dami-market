bf1539629eb14830f43dbc6eaaf513204a133e83
Ke <mezereonxp@gmail.com>
2018-07-12 23:05:58 +0800

    [UPDATE] Add login and regist component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent  } from './login/login.component';
+import { RegistComponent } from "./regist/regist.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RegistComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/regist/regist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-regist',
+  templateUrl: './regist.component.html',
+  styleUrls: ['./regist.component.scss']
+})
+export class RegistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
