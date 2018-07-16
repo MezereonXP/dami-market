3c2d10e62d523f1422cf5af1006e0ffc0f4a01a4
MezereonXP <mezereonxp@gmail.com>
2018-07-17 02:45:44 +0800

    [UPDATE] Change home to index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { KillComponent } from './kill/kill.component';
 const routes: Routes = [
   {
     path: '',
-    component: RegistComponent
+    component: HomeComponent
   },
   {
     path: 'login/:id',
