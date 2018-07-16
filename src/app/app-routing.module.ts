0eb4420609c8336d4752620ef5b3dd9a98f27c20
MezereonXP <mezereonxp@gmail.com>
2018-07-17 02:04:37 +0800

    [UPDATE] Test jenkins

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { LoginComponent  } from './login/login.component';
+import { LoginComponent } from './login/login.component';
 import { RegistComponent } from "./regist/regist.component";
 import { HomeComponent } from './home/home.component';
 import { InfomationComponent } from './infomation/infomation.component';
@@ -32,7 +32,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
