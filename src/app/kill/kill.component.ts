d5d6af4ee4b46db49b7c305c3118f52ee74dffd3
1637994421 <1637994421@qq.com>
2018-07-16 22:42:17 +0800

    [FIX] Add new time

--- src/app/kill/kill.component.ts
+++ src/app/kill/kill.component.ts
@@ -42,23 +42,23 @@ export class KillComponent implements OnInit {
       if (hour >= 16 && hour < 18) {
         this.timeTipe[0] = "距离开始还剩" + (hour - 16) + ":" + minute + ":" + second;
       }
-      if (hour >= 12 && hour < 14) {
-        this.timeTipe[1] = "距离开始还剩" + (hour - 12) + ":" + minute + ":" + second;
-      }
-      if (hour >= 8 && hour < 10) {
-        this.timeTipe[2] = "距离开始还剩" + (hour - 8) + ":" + minute + ":" + second;
-      }
-      if (hour >= 4 && hour < 6) {
-        this.timeTipe[3] = "距离开始还剩" + (hour - 4) + ":" + minute + ":" + second;
-      }
-      if (hour >= 0 && hour < 2) {
-        this.timeTipe[4] = "距离开始还剩" + hour + ":" + minute + ":" + second;
-      }
     } else {
       for (let i = 0; i < timeSlot + 1; i++) {
         this.timeTipe[i] = "已结束";
       }
 
+    }
+    if (hour >= 12 && hour < 14) {
+      this.timeTipe[1] = "距离开始还剩" + (hour - 12) + ":" + minute + ":" + second;
+    }
+    if (hour >= 8 && hour < 10) {
+      this.timeTipe[2] = "距离开始还剩" + (hour - 8) + ":" + minute + ":" + second;
+    }
+    if (hour >= 4 && hour < 6) {
+      this.timeTipe[3] = "距离开始还剩" + (hour - 4) + ":" + minute + ":" + second;
+    }
+    if (hour >= 0 && hour < 2) {
+      this.timeTipe[4] = "距离开始还剩" + hour + ":" + minute + ":" + second;
     }
 
     setTimeout(() => {
