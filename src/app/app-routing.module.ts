3e27724933525ad3404d335e3fdf5f4b048251bc
OoKurumi <972704223@qq.com>
2018-07-19 19:25:42 +0800

    [UPDATE] Add order content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { LoginComponent  } from './login/login.component';
 import { RegistComponent } from "./regist/regist.component";
 import { HomeComponent } from './home/home.component';
 import { ShopcarComponent } from './shopcar/shopcar.component'
+import { OrderComponent } from './order/order.component'
 import { InfomationComponent } from './infomation/infomation.component';
 
 const routes: Routes = [
@@ -24,7 +25,10 @@ const routes: Routes = [
     path:'shopcar',
     component: ShopcarComponent
   },
-
+  {
+    path:'order',
+    component: OrderComponent
+  },
   {
     path: 'info/:id',
     component: InfomationComponent
