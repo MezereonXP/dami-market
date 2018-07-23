d3ae1dd0c2eeb06bdb8c05e62a3de18116af1223
1637994421 <1637994421@qq.com>
2018-07-23 09:14:25 +0800

    [UPDATE] Add tipe

--- src/app/kill/kill.component.ts
+++ src/app/kill/kill.component.ts
@@ -18,10 +18,15 @@ export class KillComponent implements OnInit {
   thirdTime: string;
   fourthTime: string;
   fifthTime: string;
-  killGoodsList$:Array<KillGoods>;
-  killGoods$:object;
+  killGoodsList$: Array<KillGoods>;
+  killGoods$: object;
   killId: string = "name";
   isCanKill = false;
+  times = ["6:00", "10:00", "14:00", "18:00", "22:00"];
+  timeTipe = ["即将开始", "即将开始", "即将开始", "即将开始", "即将开始"];
+  isShowButtonList = [true, true, true, true, true];
+  ifkillGoodOver: string;
+  isShowButton: boolean;
 
   constructor(private data: DataService) { }
 
@@ -30,11 +35,10 @@ export class KillComponent implements OnInit {
     this.data.killGoods(this.killId).subscribe(
       result => {
         this.killGoods$ = result["data"];
-        console.log(this.killGoods$["1"][0].kgId);
       }
     ),
-    
-    this.currentTime = new Date();
+
+      this.currentTime = new Date();
     while (new Date().getTime() > this.testTime) {
       this.testTime = this.testTime + 86400000;
     }
@@ -44,8 +48,6 @@ export class KillComponent implements OnInit {
     this.getTime();
   }
 
-  times = ["6:00", "10:00", "14:00", "18:00", "22:00"];
-  timeTipe = ["即将开始", "即将开始", "即将开始", "即将开始", "即将开始"]
   getTime() {
 
     let nTime = (this.testTime - new Date().getTime()) / 1000;
@@ -54,17 +56,23 @@ export class KillComponent implements OnInit {
     let second = Math.floor(nTime % 86400 % 3600 % 60);
     let timeSlot = 4 - Math.floor(hour / 4);
     if (timeSlot > 0) {
-      for (let i = 0; i < timeSlot; i++) {
+      for (let i = 0; i < timeSlot-1; i++) {
         this.timeTipe[i] = "已结束";
       }
+      this.timeTipe[timeSlot-1]="正在抢购";
     }
     let temp = 4 * Math.floor(hour / 4);
     if ((hour - temp) < 2) {
       let position = Math.floor(4 - temp / 4);
       this.timeTipe[position] = "距开始还剩" + (hour - temp) + ":" + minute + ":" + second;
     }
+    if (nTime >= 0) {
+      for (let i = 4 - hour / 4; i <= 4; i++) {
+        this.isShowButtonList[i] = false;
+      }
+    }
 
-    if(second<=10) {
+    if (second <= 10) {
       this.isCanKill = true;
     } else {
       this.isCanKill = false;
@@ -81,10 +89,13 @@ export class KillComponent implements OnInit {
   }
 
   showKillGood(index) {
-    this.killGoodsList$=this.killGoods$[index];
+    this.killGoodsList$ = this.killGoods$[index];
+    this.ifkillGoodOver = "立即抢购";
+    this.isShowButton = this.isShowButtonList[index - 1];
+    window.alert(this.isShowButton);
   }
 
-  kill(){
+  kill() {
     this.data.killGoods(this.killId).subscribe(
       result => window.alert(result["msg"])
     );
