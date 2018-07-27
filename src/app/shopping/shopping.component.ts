17b1df816d355cc2b3efb7c47505704490ff8d0a
MezereonXP <mezereonxp@gmail.com>
2018-07-27 15:50:23 +0800

    [UPDATE] Add change pic by click button

--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -16,6 +16,7 @@ export class ShoppingComponent implements OnInit {
   goods: Good = new Good(0, "", "", 0, "", null);
   config: Array<Config> = new Array<Config>();
   goodimg: GoodImg = new GoodImg(0, 0, "");
+  showPic = "http://www.hxbus.net/wiki/images/4/4e/%E6%9A%82%E6%97%A0%E5%9B%BE%E5%83%8F.jpg";
 
 
   constructor(private data: DataService, private route: ActivatedRoute) { }
@@ -26,8 +27,14 @@ export class ShoppingComponent implements OnInit {
       result => this.goods = result["data"]
     );
     this.data.getShopGoodInfo().subscribe(
-      result => this.config = result["data"].config
+      result => {
+        this.config = result["data"].config;
+        this.showPic = this.config[0].goodimg[0].gi_img;
+      }
     );
+  }
 
+  changePic(index) {
+    this.showPic = this.config[index].goodimg[0].gi_img;
   }
 }
