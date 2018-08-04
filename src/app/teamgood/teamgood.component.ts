da6ddcdc55807554a325693f8554a8c8b764ce85
qq1774881426 <15804046286@163.com>
2018-08-04 17:37:02 +0800

    [UPDATE] Update addteam

--- src/app/teamgood/teamgood.component.ts
+++ src/app/teamgood/teamgood.component.ts
@@ -13,6 +13,7 @@ import { Customer } from '../bean/customer';
 import { Address } from '../bean/address';
 import { OrderGoods } from '../bean/ordergoods';
 import { Good } from '../bean/good';
+import { OrderService } from '../data/order.service';
 
 @Component({
   selector: 'app-teamgood',
@@ -30,7 +31,7 @@ export class TeamgoodComponent implements OnInit {
   good: Good = new Good(null, null, null, null, null, null, null);
   orderGoods: OrderGoods = new OrderGoods(null, this.newOrder, this.good, this.TeamGood.nowPrice, 1, 1);
   orderGoodsList: Array<OrderGoods>
-  constructor(private data: DataService, public dialog: MatDialog, public route: ActivatedRoute, private router: Router) { }
+  constructor(private data: DataService, public dialog: MatDialog, private orderService: OrderService, public route: ActivatedRoute, private router: Router) { }
 
   phone = "";// 用户的手机号
   isLogin: boolean = false;// 是否登陆
@@ -62,10 +63,19 @@ export class TeamgoodComponent implements OnInit {
       result => {
         this.Teams$ = result["data"];
       });
-    this.data.getGoodsByTgId(tgId).subscribe(
-      result => {
-        this.good = result["data"];
-      });
+      this.data.getCustomerById(this.cId).subscribe(
+        result => {
+          this.orderGoods.order.customer = result["data"];
+        }
+      );
+      this.data.getTeamGoodById(tgId).subscribe(
+        result => {
+          this.orderGoods.ogPrice = result["data"].nowPrice;
+        });
+      this.data.getGoodsByTgId(tgId).subscribe(
+        result => {
+          this.orderGoods.goods = result["data"];
+        });
   }
 
   initCId() {
@@ -81,13 +91,13 @@ export class TeamgoodComponent implements OnInit {
     this.orderGoodsList.push(this.orderGoods);
     let tgId = this.route.snapshot.paramMap.get("tgId");
     //生成团
-    this.data.insertTeam(tgId, this.cId).subscribe();
-    //递送order
-    this.router.navigate(['order'], {
-      queryParams: {
-        orderGoodsList: JSON.stringify(this.orderGoodsList)
-      }
-    })
+    this.data.insertTeam(tgId).subscribe();
+    this.orderGoodsList.push(this.orderGoods);
+    window.alert("参团成功");
+    //递送order方法
+    this.orderService.orderGoodsList = this.orderGoodsList;
+    this.orderService.tId = this.tId;
+    this.router.navigate(['order']);
   }
 
   openDialog(tId) {
